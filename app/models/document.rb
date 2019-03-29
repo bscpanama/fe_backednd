f0d5e5dc6e6fbc11645910cde6a46e14144b14bc
Ivan Granados <chivox.ivan@gmail.com>
2019-03-28 19:54:06 -0500

    changed how the days query is made

--- app/models/document.rb
+++ app/models/document.rb
@@ -14,7 +14,7 @@ class Document < ApplicationRecord
   scope :by_status, -> status { where(status: status) }
   scope :by_date, -> fecha_de_emision { where(fecha_de_emision: fecha_de_emision) }
   scope :by_period, -> started_at, ended_at { where(fecha_de_emision: started_at..ended_at) }
-  scope :by_days, -> days { where(fecha_de_emision: Date.today..days.to_i.days.from_now) }
+  scope :by_days, -> days { where(fecha_de_emision: days.to_i.days.ago.midnight..Date.today.midnight) }
 
   validates :documento_xml, blob: { content_type: ['text/xml', 'application/xml'], size_range: 1..2.megabytes }
   validates :qr_code, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..2.megabytes }
