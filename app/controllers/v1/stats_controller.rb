77b9a80303d2a5495d1b9fb294d8e09773256b26
Ivan <chivox.ivan@gmail.com>
2020-07-03 17:07:48 -0500

    custom code to provide stats

--- app/controllers/v1/stats_controller.rb
+++ app/controllers/v1/stats_controller.rb
@@ -3,7 +3,11 @@ module V1
     before_action :valid_query, only: :index
 
     def index
-      @stats = document_query.analytics(search_params[:query].to_sym, on: 'fecha_de_emision')
+      @stats = if search_params[:query] == "monthly"
+                 get_results(decument_query)
+               else
+                 document_query.analytics(search_params[:query].to_sym, on: 'fecha_de_emision')
+               end
       json_response(@stats)
     end
 
@@ -14,12 +18,31 @@ module V1
     end
 
     def valid_query
-      ["weekly","monthly", "yearly"].include? search_params[:query]
+      %w[weekly monthly yearly].include? search_params[:query]
     end
 
     def document_query
-      current_user.kind_of?(Admin) ? Document.all : Document.where(user: current_user)
+      current_user.is_a?(Admin) ? Document.all : Document.where(user: current_user)
     end
 
+    def get_results(query)
+      dates = query.map(&:fecha_de_emision).compact
+      processed = []
+      results = {}
+
+      dates.each do |date|
+        month = Date::MONTHNAMES[date.month]
+        year = date.year
+        count = 0
+        next if processed.include?("#{month},#{year}")
+
+        processed << "#{month},#{year}"
+        dates.each do |date_c|
+          count += 1 if month == Date::MONTHNAMES[date_c.month] && year == date_c.year
+        end
+        results["#{month} #{year}".to_sym] = count
+      end
+      results
+    end
   end
 end
